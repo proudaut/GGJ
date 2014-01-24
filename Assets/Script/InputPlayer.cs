37b7b8c00c04371cb862a3ce849cabbd2217bdda
Vincent Falduto <vincent.falduto@gmail.com>
2014-01-25 00:57:49 +0100

    Added inputMove script for player (player.prefab should be rename Troll)

--- Assets/Script/InputPlayer.cs
+++ Assets/Script/InputPlayer.cs
@@ -1,32 +1,21 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
 using System.Collections;
 
 public class InputPlayer : Player
 {
+	public float mRotation;
 	string mPlayer_joy;
+
 	public InputPlayer (int _id, int _type , string _inputname) : base(_id, _type)		
 	{		
 		mPlayer_joy = _inputname;
 	}
 
-	void FixedUpdate ()
+	public override void StartGame()
 	{
-		if(mGamePlayer != null)
-		{
-			//TODO Move GameObject
-			/*float moveHorizontal = Input.GetAxis (player_joy + "_DirectionHorizontal");
-			float moveVertical = Input.GetAxis (player_joy + "_DirectionVertical");
-			
-			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-			rigidbody.velocity = movement * speed;
-			
-			float rHorizontal = Input.GetAxis (player_joy + "_CameraHorizontal");
-			rigidbody.MoveRotation(Quaternion.Euler (0.0f, rHorizontal * rotation, 0.0f));
-			
-			if (Input.GetButtonDown(player_joy + "_ButtonX"))
-			{
-				Debug.Log(player_joy + "button X");
-			}*/
-		}
+		Debug.Log ("create player object");
+		//TO DO CREATE MONOBEAVIOR
+		mGamePlayer = GameObject.Instantiate (Resources.Load ("Player")) as GameObject;
+		mGamePlayer.GetComponent<inputMove> ().player_joy = mPlayer_joy;
 	}
 }
