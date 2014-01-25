69d110a5525eaec79b39849cc79582cbf911f1f5
Vincent Falduto <vincent.falduto@gmail.com>
2014-01-25 04:45:05 +0100

    Added shpere represent trolll head. Fixed rotation when move

--- Assets/Script/inputMove.cs
+++ Assets/Script/inputMove.cs
@@ -26,10 +26,14 @@ public class inputMove : MonoBehaviour {
 			
 			Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
 			rigidbody.velocity = movement * speed;
+
+			transform.up = new Vector3 (moveHorizontal, moveVertical, 0.0f).normalized;
 			
 			float rHorizontal = Input.GetAxis (player_joy + "_CameraHorizontal");
 			float rVertical = Input.GetAxis (player_joy + "_CameraVertical");
-			transform.up = new Vector3 (-rHorizontal, rVertical, 0.0f).normalized;
+			transform.up = new Vector3 (rHorizontal, -rVertical, 0.0f).normalized;
+
+
 			//rigidbody.MoveRotation(Quaternion.Euler (0.0f, 0.0f, -qrHorizontal * rotation));
 			
 			if (Input.GetButtonDown(player_joy + "_ButtonX")){
