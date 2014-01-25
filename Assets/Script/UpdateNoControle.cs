9b5fa211ed554fc1ea2c1f269ff3102dd802d533
Roudaut Prigent <prigent.roudaut@c4mprod.com>
2014-01-25 04:02:38 +0100

    server client management

---
+++ Assets/Script/UpdateNoControle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateNoControle : MonoBehaviour 
+{
+	public Player mPlayer = null;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(mPlayer!= null)
+		{
+			this.gameObject.transform.position = new Vector3(mPlayer.mx , mPlayer.my , mPlayer.mz);
+		}
+	}
+}
