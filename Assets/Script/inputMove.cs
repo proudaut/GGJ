1324acadc8f4d39288a4f30482c5891edefeb386
Vincent Falduto <vincent.falduto@gmail.com>
2014-01-25 03:04:22 +0100

    Fix merge for inputMove

--- Assets/Script/inputMove.cs
+++ Assets/Script/inputMove.cs
@@ -14,18 +14,9 @@ public class inputMove : MonoBehaviour {
 	}
 	public string Getplayer_joy(string _player_joy)
 	{
-<<<<<<< HEAD
-		float moveHorizontal = Input.GetAxis (player_joy + "_DirectionHorizontal");
-		float moveVertical = Input.GetAxis (player_joy + "_DirectionVertical");
-		
-		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
-		rigidbody.velocity = movement * speed;
-=======
 		return player_joy;
 	}
->>>>>>> 46ad1a80db0dba569698f368cbde64d3c7b62d39
-
-
+	
 	void FixedUpdate ()
 	{
 		if(player_joy != null)
