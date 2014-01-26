b81eb741e27302c5c9cecf56a4075772e5556270
Vincent Falduto <vincent.falduto@gmail.com>
2014-01-26 14:22:12 +0100

    Added script move

---
+++ Assets/Controls/DirectionMoveAcc.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionMoveAcc : MonoBehaviour {
+
+	public float speed = 3f;
+	void Update () {
+		Vector3 movement = new Vector3 (Input.acceleration.x, -Input.acceleration.y, 0.0f);
+		movement *= Time.deltaTime;
+
+		// clamp acceleration vector to unit sphere
+		if (movement.sqrMagnitude > 1)
+			movement.Normalize();
+
+		transform.Translate (movement * speed);	
+	}
+}
