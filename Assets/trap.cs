968be9fb95e7bf7800095e15524ac80dd0dd133f
jlevet <levet.jerome@gmail.com>
2014-01-26 03:06:35 +0100

    water

---
+++ Assets/trap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trap : MonoBehaviour 
+{
+	public GameObject Target;
+
+	/*void OnCollisionEnter(Collision coll)
+	{
+		Debug.Log("Collision trap");
+		if(coll.gameObject.layer == LayerMask.NameToLayer("Goblin") || coll.gameObject.layer == LayerMask.NameToLayer("Troll"))
+		{
+			Debug.Log("Collision trap with goblin");
+			Target.GetComponentInChildren<Animation>().Play("WaterBounce");
+		}
+	}*/
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if(coll.gameObject.layer == LayerMask.NameToLayer("Goblin") || coll.gameObject.layer == LayerMask.NameToLayer("Troll"))
+		{
+			Target.GetComponentInChildren<Animation>().Play();
+		}
+	}
+}
